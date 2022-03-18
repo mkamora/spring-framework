49ae3dc983c90c3c1755be94e5d9e49356af8098
mkamora <mk.amoorah@gmail.com>
2022-03-18 09:15:34 -0400

    Added Module 8 MVC-Model-Thymeleaf

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.cydeo.controller;
 
+import com.cydeo.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -13,8 +18,28 @@ public class StudentController {
         //model methods
         model.addAttribute("name", "Cydeo");
         model.addAttribute("course", "MVC");
-        return "/student/welcome";
 
+
+        String subject = "Collections";
+
+        model.addAttribute("subject", subject);
+
+        //Create some random studentId(0-1000) and show it in your UI
+        Random ran = new Random();
+        int randomNumber = ran.nextInt(1000);
+        model.addAttribute("id", randomNumber);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        model.addAttribute("numbers", numbers);
+
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
+
+        return "/student/welcome";
     }
 
 }
