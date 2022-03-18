462437319a767504d10e17a8be249b1d7ba16455
mkamora <mk.amoorah@gmail.com>
2022-03-18 10:38:38 -0400

    Added PathVariable to Controller

--- Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -2,6 +2,7 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -28,5 +29,16 @@ public class CarController {
         System.out.println(year);
         return "/car/car-information";
     }
+
+    @RequestMapping("/info3/{make}/{year}")
+    public String getCarInfo(@PathVariable String make, @PathVariable Integer year, Model model) {
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+        System.out.println(make);
+        System.out.println(year);
+        return "/car/car-information";
+
+    }
+
 
 }
