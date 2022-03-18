e9dd3c76e5f9399e23f3d391aafce7ef4717e419
mkamora <mk.amoorah@gmail.com>
2022-03-18 10:22:36 -0400

    Added RequestParm to Controllers

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/cydeo/controller/CarController.java
@@ -0,0 +1,32 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+    @RequestMapping("/info")
+    public String carInfo(@RequestParam String make, @RequestParam Integer year, Model model) {
+
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+        System.out.println(make);
+        System.out.println(year);
+        return "/car/car-information";
+    }
+
+    @RequestMapping("/info2")
+    public String carInfo2(@RequestParam(value = "make", required = false, defaultValue = "Tesla") String make, @RequestParam Integer year, Model model) {
+
+        model.addAttribute("make", make);
+        model.addAttribute("year", year);
+        System.out.println(make);
+        System.out.println(year);
+        return "/car/car-information";
+    }
+
+}
